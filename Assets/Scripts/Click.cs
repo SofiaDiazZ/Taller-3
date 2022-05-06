b1f6fa36f10c47012bc2ad6e517b4c08bdc1ea95
Sofía Díaz Z <67989042+SofiaDiazZ@users.noreply.github.com>
2022-05-05 21:22:55 -0500

    Script buttonClick

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -13,6 +13,11 @@ public class Click : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if(Input.GetMouseButtonDown(0))
+       {
+           cambiaColor();
+       }
+       */
     }
 }
